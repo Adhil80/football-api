f554606a218764b8cd0dbc96b00273d64cd2027f
Your Name <you@example.com>
2021-11-23 08:32:49 +0530

    1.3

--- helpers/fetch-mathes.js
+++ helpers/fetch-mathes.js
@@ -81,10 +81,10 @@ module.exports = {
                                 if (b.date == 'Tomorrow') {
                                     bDate.setUTCDate(bDate.getUTCDate() + 1)
                                 }
-                                aDate.setHours(a.time24.split(':')[0]+6)
+                                aDate.setHours(a.time24.split(':')[0])
                                 aDate.setMinutes(a.time24.split(':')[1]+2)
 
-                                bDate.setHours(b.time24.split(':')[0]+6)
+                                bDate.setHours(b.time24.split(':')[0])
                                 bDate.setMinutes(b.time24.split(':')[1]+2)
 
                                 
--- test.js
+++ test.js
@@ -1,12 +1,1 @@
-const jsObjects = [
-    {a: 1, b: 2}, 
-    {a: 3, b: 4}, 
-    {a: 5, b: 6}, 
-    {a: 7, b: 8}
-  ]
-  
-  let result = jsObjects.filter(obj => {
-    return obj.b === 21
-  })
-  
-  console.log(result)
+console.log(new Date());
