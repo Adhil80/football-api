bac5d933ad8455555db291950ac2d4606d9d4244
Your Name <you@example.com>
2021-12-22 17:49:29 +0530

    3.0

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 let express = require('express');
+global.TextEncoder = require("util").TextEncoder;
+
+
 const { fetchMatches } = require('./helpers/fetch-mathes');
 const onStartConfig = require('./onStartConfig');
 let router = express()
+
 router.listen(process.env.PORT || 5000, () => {
     console.log(`server started at port 5000\nurl:http://localhost:5000`);
 })
