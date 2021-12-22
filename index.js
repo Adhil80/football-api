5f91004df080dd5bead6b610525effff47ad1421
Your Name <you@example.com>
2021-12-22 17:50:46 +0530

    3.0

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let express = require('express');
 global.TextEncoder = require("util").TextEncoder;
-
+require('core-js/stable')
 
 const { fetchMatches } = require('./helpers/fetch-mathes');
 const onStartConfig = require('./onStartConfig');
